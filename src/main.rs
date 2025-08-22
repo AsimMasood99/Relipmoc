56f0f255d8e3b8439f828fed18e4dd54a6b3048b
Mubashir22009 <bscs22009@itu.edu.pk>
2025-08-22 16:32:29 +0500

    single file for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::fs;
-use std::io;
-use std::io::Write;
 use std::path::Path;
 
 fn get_code() -> String {
-    let mut file_name = String::new();
-    print!("Enter the file name: ");
-    io::stdout().flush().unwrap(); // flush to print on screen
-    io::stdin().read_line(&mut file_name).unwrap();
+    // TODO: allow custom file path
+    // let mut file_name = String::new();
+    // print!("Enter the file name: ");
+    // io::stdout().flush().unwrap(); // flush to print on screen
+    // io::stdin().read_line(&mut file_name).unwrap();
 
-    let path = Path::new("data").join(file_name.trim());
+    let path = Path::new("data").join("code.txt");
     let file_content = fs::read_to_string(path).unwrap();
 
     return file_content;
