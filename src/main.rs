39615150b159ed93033a85f8b7e18be2f51b4638
Mubashir22009 <bscs22009@itu.edu.pk>
2025-08-22 16:23:28 +0500

    get code to lex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use std::io;
+use std::io::Write;
+use std::path::Path;
+
+fn get_code() -> String {
+    let mut file_name = String::new();
+    print!("Enter the file name: ");
+    io::stdout().flush().unwrap(); // flush to print on screen
+    io::stdin().read_line(&mut file_name).unwrap();
+
+    let path = Path::new("data").join(file_name.trim());
+    let file_content = fs::read_to_string(path).unwrap();
+
+    return file_content;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let code = get_code();
+    println!("{code}");
 }
